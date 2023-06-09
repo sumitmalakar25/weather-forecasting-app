2fb6bba902805bee3d71598fd69134ae43287b44
Sumit Malakar <sumitmalakar025@gmail.com>
2023-06-09 13:56:23 +0530

    Update : Cityerror.jsx

--- src/components/Cityerror.jsx
+++ src/components/Cityerror.jsx
@@ -4,7 +4,9 @@ import img1 from './assets/error.gif'
 function Cityerror() {
   return (
     <div>
-            <img src={img1} style={{marginTop:'-1px'}} height={350} width={300}/>
+      
+      <img src={img1} alt="Error: City Not Found" style={{marginTop:'-1px'}} height={350} width={300}/>
+
     </div>
   );
 }
